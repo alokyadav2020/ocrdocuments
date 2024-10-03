b77ca208363e3f1cdd9b3f57b69ae9af93408744
unknown <alokranjan.ucer@gmail.com>
2024-10-03 16:13:37 +0530

    app

---
+++ application.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import os
+import uuid
+from src.documentsai import ocr_doc
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+
+# Load the .env file
+load_dotenv()
+
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+LOCATION =  os.getenv('LOCATION')  
+PROCESSOR_ID = os.getenv('PROCESSOR_ID') 
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcs_connections"])
+
+
+# Define the folder to save the uploaded files
+UPLOAD_FOLDER = "uploaded_files"
+
+# Ensure the folder exists
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Function to delete existing files in the folder
+def delete_existing_files(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)  # Delete the file
+        except Exception as e:
+            st.error(f"Error deleting file {file_path}: {e}")
+
+# Streamlit file upload component
+uploaded_file = st.file_uploader("Choose an image...", type=["jpeg"])
+
+if uploaded_file is not None:
+    # Delete any existing file before saving the new one
+    delete_existing_files(UPLOAD_FOLDER)
+    
+    # Generate a unique filename
+    unique_filename = str(uuid.uuid4()) + "_" + uploaded_file.name
+    
+    # Define the save path
+    save_path = os.path.join(UPLOAD_FOLDER, unique_filename)
+    
+    # Save the file in the folder
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+
+    # col1, col2 = st.columns([2,2])    
+
+    result = ocr_doc(PROJECT_ID,LOCATION,PROCESSOR_ID,save_path,credentials) 
+    st.write(result)
+
+
+    # with col1:
+    #  st.image(save_path, caption="Uploaded Image", use_column_width=True)
+
+    # with col2:
+    #  st.write(result)
+    
+    # Confirmation message
+    # st.success(f"File saved as {unique_filename}")
+    
+    # # Display the uploaded image
+    # st.image(save_path, caption="Uploaded Image", use_column_width=True)
---
+++ src/documentsai.py
@@ -0,0 +1,47 @@
+from google.api_core.client_options import ClientOptions
+from google.cloud import documentai
+
+from pathlib import Path
+
+
+
+MIME_TYPE ='image/jpeg'
+
+
+def ocr_doc(PROJECT_ID:str,LOCATION:str,PROCESSOR_ID:str,FILE_PATH:Path,credentials):
+
+    try:
+       
+       if FILE_PATH is not None:
+           #Instantiates a client
+        docai_client = documentai.DocumentProcessorServiceClient(client_options=ClientOptions(api_endpoint=f"{LOCATION}-documentai.googleapis.com"),credentials=credentials)
+
+        # The full resource name of the processor, e.g.:
+        # projects/project-id/locations/location/processor/processor-id
+        # You must create new processors in the Cloud Console first
+        RESOURCE_NAME = docai_client.processor_path(PROJECT_ID, LOCATION, PROCESSOR_ID)
+
+        # Read the file into memory
+        with open(FILE_PATH, "rb") as image:
+            image_content = image.read()
+
+        # Load Binary Data into Document AI RawDocument Object
+        raw_document = documentai.RawDocument(content=image_content, mime_type=MIME_TYPE)
+
+        # Configure the process request
+        request = documentai.ProcessRequest(name=RESOURCE_NAME, raw_document=raw_document)
+
+        # Use the Document AI client to process the sample form
+        result = docai_client.process_document(request=request)
+
+        document_object = result.document
+        return document_object.text
+        # print("Document processing complete.")
+        # print(f"Text: {document_object.text}")
+                
+       
+    except Exception as e:
+       raise e
+       
+
+#
